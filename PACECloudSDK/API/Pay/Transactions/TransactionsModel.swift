4076ffb242a1471c9a8bca871d42bf734dd4c907
Martin Dinh <martin.dinh@pace.car>
2020-12-01 16:26:33 +0100

    Make all transaction model's attributes optional

--- PACECloudSDK/API/Pay/Transactions/TransactionsModel.swift
+++ PACECloudSDK/API/Pay/Transactions/TransactionsModel.swift
@@ -13,26 +13,26 @@ public extension API.Pay {
     }
 
     struct Transaction: Decodable {
-        public let type: String
-        public let id: String
-        public let attributes: TransactionAttributes
-        public let links: TransactionLinks
+        public let type: String?
+        public let id: String?
+        public let attributes: TransactionAttributes?
+        public let links: TransactionLinks?
     }
 
     struct TransactionAttributes: Decodable {
-        public let vat: TransactionVAT
-        public let createdAt: String
-        public let currency: String
-        public let fuel: TransactionFuel
-        public let location: TransactionLocation
-        public let paymentMethodId: String
-        public let paymentMethodKind: String
-        public let paymentToken: String
-        public let priceIncludingVAT: Double
-        public let priceWithoutVAT: Double
-        public let providerPRN: String
-        public let purposePRN: String
-        public let updatedAt: String
+        public let vat: TransactionVAT?
+        public let createdAt: String?
+        public let currency: String?
+        public let fuel: TransactionFuel?
+        public let location: TransactionLocation?
+        public let paymentMethodId: String?
+        public let paymentMethodKind: String?
+        public let paymentToken: String?
+        public let priceIncludingVAT: Double?
+        public let priceWithoutVAT: Double?
+        public let providerPRN: String?
+        public let purposePRN: String?
+        public let updatedAt: String?
 
         enum CodingKeys: String, CodingKey { // swiftlint:disable:this nesting
             case vat = "VAT"
@@ -43,45 +43,45 @@ public extension API.Pay {
     }
 
     struct TransactionVAT: Decodable {
-        public let amount: Double
-        public let rate: Double
+        public let amount: Double?
+        public let rate: Double?
     }
 
     struct TransactionFuel: Decodable {
-        public let amount: Double
-        public let pricePerUnit: Int
-        public let productName: String
-        public let pumpNumber: Int
+        public let amount: Double?
+        public let pricePerUnit: Double?
+        public let productName: String?
+        public let pumpNumber: Int?
     }
 
     struct TransactionLocation: Decodable {
-        public let address: TransactionAddress
-        public let brand: String
-        public let latitude: Double
-        public let longitude: Double
+        public let address: TransactionAddress?
+        public let brand: String?
+        public let latitude: Double?
+        public let longitude: Double?
     }
 
     // MARK: - Address
     struct TransactionAddress: Decodable {
-        public let city: String
-        public let countryCode: String
-        public let houseNo: String
-        public let postalCode: String
-        public let street: String
+        public let city: String?
+        public let countryCode: String?
+        public let houseNo: String?
+        public let postalCode: String?
+        public let street: String?
     }
 
     struct TransactionLinks: Decodable {
-        public let receipt: TransactionReceipt
-        public let receiptPDF: TransactionReceipt
+        public let receipt: TransactionReceipt?
+        public let receiptPDF: TransactionReceipt?
     }
 
     struct TransactionReceipt: Decodable {
-        public let href: String
-        public let meta: TransactionMeta
+        public let href: String?
+        public let meta: TransactionMeta?
     }
 
     struct TransactionMeta: Decodable {
-        public let mimeType: TransactionMIMEType
+        public let mimeType: TransactionMIMEType?
     }
 
     enum TransactionMIMEType: String, Decodable {
