0862b63aac1e1e0a451ff9ac5a2939a5096e5764
Martin Dinh <martin.dinh@pace.car>
2021-01-22 09:55:00 +0100

    Adjust QueryParamHandler to ignore set of URLs

--- PACECloudSDK/Utils/QueryParamHandler.swift
+++ PACECloudSDK/Utils/QueryParamHandler.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 class QueryParamHandler {
+    private static let ignoredUrls: [String] = [
+        Settings.shared.baseUrl(.search),
+        Settings.shared.baseUrl(.reverseGeocode),
+        Settings.shared.baseUrl(.osrm)
+    ]
+
     static func buildUrl(for url: URL) -> URL? {
-        guard var urlComponents = URLComponents(string: url.absoluteString) else { return nil }
+        guard !ignoredUrls.contains(where: url.absoluteString.contains) else { return url }
+
+        guard var urlComponents = URLComponents(string: url.absoluteString) else { return url }
 
         let queryItems: [URLQueryItem] = urlComponents.queryItems ?? []
 
