0b871d6d93d6e678354fc07f94ed0d5494fcabc0
Patrick Niepel <patrick@pace.car>
2021-02-04 09:57:32 +0100

    Generate geojson api

---
+++ PACECloudSDK/API/GeoJSON/GeoJSON.swift
@@ -0,0 +1,14 @@
+//
+//  GeoJSON.swift
+//  PACECloudSDK
+//
+//  Created by PACE Telematics GmbH.
+//
+
+import Foundation
+
+public extension API {
+    struct GeoJSON {
+        public static let client: GeoJSONAPIClient = .custom
+    }
+}
