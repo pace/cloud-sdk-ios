caec7edb3d972045760786b971f79ef8714d4f12
Talip Göksu <talip@pace.car>
2021-02-04 12:39:50 +0100

    Add user api

---
+++ PACECloudSDK/API/User/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  PACECloudSDK
+//
+//  Created by PACE Telematics GmbH.
+//
+
+import Foundation
+
+public extension API {
+    struct User {
+        public static let client: UserAPIClient = .custom
+    }
+}
--- PACECloudSDK/PACECloudSDK.swift
+++ PACECloudSDK/PACECloudSDK.swift
@@ -37,6 +37,8 @@ public class PACECloudSDK {
 
         API.POI.client.baseURL = Settings.shared.baseUrl(.poiApi)
         API.Pay.client.baseURL = Settings.shared.baseUrl(.payApi)
+        API.Fueling.client.baseURL = Settings.shared.baseUrl(.fuelingApi)
+        API.User.client.baseURL = Settings.shared.baseUrl(.userApi)
 
         API.POI.client.defaultHeaders = [HttpHeaderFields.userAgent.rawValue: Bundle.paceCloudSDK.poiKitUserAgent,
                                          HttpHeaderFields.apiKey.rawValue: apiKey ?? "Missing API key"]
