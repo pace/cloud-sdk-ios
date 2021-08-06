c67b04e49150b10a29e02b93ad0e4061daf5df84
Patrick Niepel <patrick@pace.car>
2021-08-06 12:48:14 +0200

    Add convenient methods for payment methods, transactions and pin validity

---
+++ PACECloudSDKTests/IDKitTests/PINValidityTests.swift
@@ -0,0 +1,27 @@
+//
+//  PINValidityTests.swift
+//  PACECloudSDKTests
+//
+//  Created by PACE Telematics GmbH.
+//
+
+import XCTest
+@testable import PACECloudSDK
+
+class PINValidityTests: XCTestCase {
+    override func setUpWithError() throws {
+        PACECloudSDK.shared.setup(with: .init(apiKey: "apiKey", environment: .development))
+    }
+
+    func testValidPINs() {
+        ["1235", "9875", "1012", "1123", "4378"].forEach { pin in
+            XCTAssertTrue(IDKit.isPINValid(pin: pin))
+        }
+    }
+
+    func testInvalidPINs() {
+        ["1234", "9012", "0987", "6543", "1122", "1111", "123", "456123"].forEach { pin in
+            XCTAssertFalse(IDKit.isPINValid(pin: pin))
+        }
+    }
+}
