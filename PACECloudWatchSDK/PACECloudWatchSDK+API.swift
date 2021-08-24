b4e408aa76c06fc19fdf8797888fa9c988a4a3f2
Patrick Niepel <patrick@pace.car>
2021-08-24 16:17:27 +0200

    Set cms base url for watch sdk

--- PACECloudWatchSDK/PACECloudWatchSDK+API.swift
+++ PACECloudWatchSDK/PACECloudWatchSDK+API.swift
@@ -13,6 +13,7 @@ extension PACECloudSDK {
         API.Pay.client.baseURL = Settings.shared.baseUrl(.payApi)
         API.Fueling.client.baseURL = Settings.shared.baseUrl(.fuelingApi)
         API.User.client.baseURL = Settings.shared.baseUrl(.userApi)
+        API.Cms.client.baseURL = Settings.shared.baseUrl(.cms)
 
         API.POI.client.defaultHeaders = [HttpHeaderFields.userAgent.rawValue: Bundle.paceCloudSDK.poiKitUserAgent,
                                          HttpHeaderFields.apiKey.rawValue: apiKey ?? "Missing API key"]
