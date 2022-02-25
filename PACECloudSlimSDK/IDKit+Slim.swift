034921e7e62a85cab75f60257f20040c04935e2c
Patrick Niepel <patrick@pace.car>
2022-02-25 11:49:07 +0100

    chore: add IDKit to PACECloudSlimSDK

--- PACECloudSlimSDK/IDKit+Slim.swift
+++ PACECloudSlimSDK/IDKit+Slim.swift
@@ -7,17 +7,36 @@
 
 import Foundation
 
-public struct IDKit {
-    public struct OIDConfiguration {}
+extension IDKit {
+    func performApiInducedRefresh(_ completion: @escaping (Bool) -> Void) {}
+    static func performDiscovery(issuerUrl: String, _ completion: @escaping (Result<OIDConfiguration.Response, IDKitError>) -> Void) {}
+}
 
-    static var isSessionAvailable: Bool {
-        false
-    }
+extension IDKit {
+    func isPasswordSet(completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func isPINSet(completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func isPINOrPasswordSet(completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func isPINValid(pin: String) -> Bool { false }
+    func setPIN(pin: String, password: String, completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func setPINWithBiometry(pin: String, completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func setPIN(pin: String, otp: String, completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func isBiometricAuthenticationEnabled() -> Bool { false }
+    func evaluateBiometryPolicy(completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+    func enableBiometricAuthentication(pin: String?, password: String?, otp: String?, completion: ((Result<Bool, IDKitError>) -> Void)?) {}
+    func disableBiometricAuthentication() {}
+    func otp(for password: String, completion: @escaping (Result<String, IDKitError>) -> Void) {}
+    func otpWithBiometry(completion: @escaping (Result<String, IDKitError>) -> Void) {}
+    func sendMailOTP(completion: @escaping (Result<Bool, IDKitError>) -> Void) {}
+}
 
-    static func determineOIDConfiguration(with customOIDConfig: OIDConfiguration? = nil) {}
-    static func apiInducedRefresh(_ completion: @escaping (Bool) -> Void) {
-        completion(false)
-    }
+public extension IDKit {
+    struct UserInfo {}
+    class PCPayPaymentMethods {}
+    class PCPayTransactions {}
+}
 
-    static func handleAdditionalQueryParams(_ params: Set<URLQueryItem>) { }
+extension IDKit {
+    func userInfo(completion: @escaping (Result<UserInfo, IDKitError>) -> Void) {}
+    func paymentMethods(completion: @escaping (Result<PCPayPaymentMethods, IDKitError>) -> Void) {}
+    func transactions(completion: @escaping (Result<PCPayTransactions, IDKitError>) -> Void) {}
 }
---
+++ PACECloudSlimSDK/Keychain+Slim.swift
@@ -0,0 +1,14 @@
+//
+//  Keychain+Slim.swift
+//  PACECloudSlimSDK
+//
+//  Created by PACE Telematics GmbH.
+//
+
+import Foundation
+
+public extension PACECloudSDK {
+    class Keychain {
+        public func getData(for key: String) -> Data? { nil }
+    }
+}
