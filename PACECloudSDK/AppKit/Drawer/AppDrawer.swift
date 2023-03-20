3916e85c42cc8ee0c578636f1b2180c93cd1e70f
Talip Göksu <talip@pace.car>
2023-03-20 14:36:17 +0100

    feat: Add distance to AppDrawers when multiple apps are in range

--- PACECloudSDK/AppKit/Drawer/AppDrawer.swift
+++ PACECloudSDK/AppKit/Drawer/AppDrawer.swift
@@ -14,7 +14,7 @@ public protocol AppDrawerProtocol: AnyObject {
 
 extension AppKit {
     open class AppDrawer: UIView {
-        public let appData: AppData
+        internal(set) public var appData: AppData
         public var currentState: State = .collapsed
         public weak var delegate: AppDrawerProtocol?
         internal(set) public var appViewController: AppViewController?
@@ -35,6 +35,8 @@ extension AppKit {
 
         var appDrawerBackgroundColor: UIColor = AppStyle.lightColor
         let appIconBackgroundColor: UIColor
+        var distanceViewLeadingConstraint: NSLayoutConstraint?
+        var subtitleTrailingConstraint: NSLayoutConstraint?
 
         lazy var drawerBackgroundView: UIView = {
             let view = UIView()
@@ -57,6 +59,30 @@ extension AppKit {
             return label
         }()
 
+        lazy var distanceView: UIView = {
+            let view = UIView()
+            view.layer.cornerRadius = 4
+            return view
+        }()
+
+        lazy var subtitleContainer: UIView = {
+            let view = UIView()
+            return view
+        }()
+
+        lazy var distanceIcon: UIImageView = {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = AppStyle.distanceIcon
+            return imageView
+        }()
+
+        lazy var distanceLabel: UILabel = {
+            let label = UILabel()
+            label.font = AppStyle.regularFont(ofSize: 12)
+            return label
+        }()
+
         lazy var subtitleLabel: UILabel = {
             let label = UILabel()
             label.font = AppStyle.regularFont(ofSize: 16)
@@ -76,6 +102,13 @@ extension AppKit {
             return imageView
         }()
 
+        private lazy var metricFormatter: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .none
+            formatter.maximumFractionDigits = 0
+            return formatter
+        }()
+
         public init(with appData: AppData) {
             self.appData = appData
 
@@ -86,6 +119,7 @@ extension AppKit {
 
             titleLabel.text = appData.appManifest?.name ?? "Name Placeholder"
             subtitleLabel.text = appData.appManifest?.description ?? "Description Placeholder"
+            updateUserDistance(with: appData)
 
             setupGestureRecognizers()
 
@@ -105,6 +139,19 @@ extension AppKit {
                 drawerBackgroundView.addShadow(cornerRadius: self.bounds.height / 2, corners: [.topLeft, .bottomLeft], fillColor: appDrawerBackgroundColor)
                 didLayoutSubviews = true
             }
+        }
+
+        func update(with appData: AppData) {
+            self.appData = appData
+
+            updateUserDistance(with: appData)
+            subtitleTrailingConstraint?.isActive = appData.userDistance == nil || !appData.shouldShowDistance
+            if appData.userDistance == nil || !appData.shouldShowDistance {
+                distanceView.removeFromSuperview()
+            } else if distanceView.superview == nil {
+                layoutDistanceView()
+            }
+            layoutIfNeeded()
         }
 
         func setupView(theme: AppDrawerTheme) {
@@ -137,7 +184,11 @@ extension AppKit {
 
             titleLabel.textColor = appDrawerTextColor
             subtitleLabel.textColor = appDrawerTextColor
+            distanceView.backgroundColor = appDrawerTextColor
             closeButton.setImage(AppStyle.roundCloseIcon.tinted(with: appDrawerTextColor), for: .normal)
+
+            distanceIcon.tintColor = appDrawerBackgroundColor
+            distanceLabel.textColor = appDrawerBackgroundColor
         }
 
         func setDrawerBackgroundViewColor(with color: UIColor) {
@@ -163,6 +214,14 @@ extension AppKit {
             else { return }
 
             appImageView.load(urlString: iconUrlString.absoluteString)
+        }
+
+        private func updateUserDistance(with appData: AppData) {
+            if appData.shouldShowDistance,
+               let distance = appData.userDistance,
+               let formattedDistance = metricFormatter.string(from: NSNumber(value: distance)) {
+                distanceLabel.text = distance <= 10 ? "0 m" : "\(formattedDistance) m"
+            }
         }
 
         private func setupDesign() {
@@ -230,7 +289,7 @@ extension AppKit.AppDrawer {
         drawerWidthConstraint?.isActive = true
 
         [drawerBackgroundView, appImageBackgroundView].forEach(addSubview)
-        [titleLabel, subtitleLabel, closeButton].forEach(drawerBackgroundView.addSubview)
+        [titleLabel, subtitleContainer, closeButton].forEach(drawerBackgroundView.addSubview)
         appImageBackgroundView.addSubview(appImageView)
 
         appImageBackgroundView.anchor(top: topAnchor,
@@ -246,11 +305,13 @@ extension AppKit.AppDrawer {
                           padding: .init(top: 0, left: labelsPadding, bottom: 9.5, right: 0),
                           size: .init(width: labelWidth, height: 0))
 
-        subtitleLabel.anchor(top: drawerBackgroundView.topAnchor,
+        subtitleContainer.anchor(top: drawerBackgroundView.topAnchor,
                              leading: drawerBackgroundView.leadingAnchor,
                              bottom: titleLabel.topAnchor,
                              padding: .init(top: 9, left: labelsPadding, bottom: 4.5, right: 0),
                              size: .init(width: labelWidth, height: 0))
+
+        layoutSubtitleContainer()
 
         closeButton.anchor(centerY: drawerBackgroundView.centerYAnchor, size: .init(width: closeButtonSize, height: closeButtonSize))
         closeButton.anchor(trailing: drawerBackgroundView.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
@@ -262,6 +323,48 @@ extension AppKit.AppDrawer {
 
         let iconSize = AppStyle.iconSize
         appImageView.centerInSuperview(size: .init(width: iconSize, height: iconSize))
+    }
+
+    private func layoutSubtitleContainer() {
+        subtitleContainer.addSubview(subtitleLabel)
+        subtitleTrailingConstraint = subtitleLabel.trailingAnchor.constraint(equalTo: subtitleContainer.trailingAnchor)
+        subtitleTrailingConstraint?.isActive = !appData.shouldShowDistance
+
+        if appData.shouldShowDistance {
+            layoutDistanceView()
+        }
+
+        subtitleLabel.anchor(top: subtitleContainer.topAnchor,
+                             leading: subtitleContainer.leadingAnchor,
+                             bottom: subtitleContainer.bottomAnchor)
+    }
+
+    private func layoutDistanceView() {
+        subtitleContainer.addSubview(distanceView)
+        [distanceIcon, distanceLabel].forEach(distanceView.addSubview)
+        distanceView.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        subtitleLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        distanceLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        distanceIcon.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+
+        distanceView.trailingAnchor.constraint(lessThanOrEqualTo: subtitleContainer.trailingAnchor, constant: -16).isActive = true
+
+        distanceView.anchor(top: subtitleContainer.topAnchor,
+                            leading: subtitleLabel.trailingAnchor,
+                            bottom: subtitleLabel.bottomAnchor,
+                            padding: .init(top: 0, left: 6, bottom: 0, right: 0))
+
+        distanceIcon.anchor(top: distanceView.topAnchor,
+                            leading: distanceView.leadingAnchor,
+                            bottom: distanceView.bottomAnchor,
+                            padding: .init(top: 2, left: 4, bottom: 2, right: 0))
+
+        distanceLabel.anchor(top: distanceView.topAnchor,
+                             leading: distanceIcon.trailingAnchor,
+                             bottom: distanceView.bottomAnchor,
+                             trailing: distanceView.trailingAnchor,
+                             centerY: distanceIcon.centerYAnchor,
+                             padding: .init(top: 2, left: 4, bottom: 2, right: 4))
     }
 }
 
