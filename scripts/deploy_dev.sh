103cb41bb0f9d6be897dc95d3bbbc7f67d34925e
Patrick Niepel <patrick@pace.car>
2023-04-19 11:59:09 +0200

    ci: Correctly set branch for dev deploy job

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -9,10 +9,11 @@ echo 'Updating Package.swift file'
 echo 'Committing changes'
 git remote set-url origin "git@$CI_SERVER_HOST:$CI_PROJECT_PATH.git"
 git fetch --all --tags --force
+git checkout master
 git add -u
 git commit -m "build: Update Package file"
 git tag -f dev
-git push origin master --tags --atomic --force # Force push needed to override tag
+git push origin master --tags --atomic --force-with-lease # Force push needed to override tag
 
 echo 'Updating dev release'
 ./scripts/update_github_dev_release_assets.sh $SLIM_SDK $WATCH_SDK
